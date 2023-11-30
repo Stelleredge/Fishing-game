50d151397af69e52e8560ccbe2de2ba208a03999
Stelleredge <stelleredgeoffical@gmail.com>
2023-11-30 20:42:37 +0530

    main

---
+++ My project/Assets/sequence/Script/CameraTracker.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject hook;
+    void Update()
+    {
+        if (hook != null)
+        {
+            Vector3 newPosition = hook.transform.position;
+            newPosition.z = -10; // Set the z position of the camera
+            transform.position = newPosition;
+        }
+    }
+}
